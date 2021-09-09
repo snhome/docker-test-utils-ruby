65238a931cf8ce780f4e2b30de3eeb065e98c467
jack <ameba.jack@gmail.com>
2021-09-09 18:21:32 +0800

    fix telegram

--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -5,7 +5,7 @@ function sendMessage(){
     text="$2"
     [ -z "$chat_id" ] && echo "no action" && exit 1
     [ -z "$text" ] && echo "no action" && exit 1
-    echo "{\"chat_id\": \"${chat_id}\", \"text\": \"${text}\", \"disable_notification\": true}" 
+    echo "{\"chat_id\": \"${chat_id}\", \"text\": \"${text}\", \"disable_notification\": true}"
     curl -X POST \
         -H 'Content-Type: application/json' \
         -d "{\"chat_id\": \"${chat_id}\", \"text\": \"${text}\", \"disable_notification\": true}" \
@@ -16,4 +16,4 @@ if [ -z ${TELEGRAM_BOT_TOKEN+x} ]; then echo "set TELEGRAM_BOT_TOKEN first" && e
 action=$1
 shift
 [ -z "$action" ] && echo "no action" && exit 1
-$action $@
+$action "$@"
